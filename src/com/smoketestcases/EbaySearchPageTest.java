23226173e03f19e0cd0d18bc8978a02d6745586c
lipika08 <lipikasingh07@gmail.com>
2025-05-13 11:03:02 +0530

    commiting changes

--- src/com/smoketestcases/EbaySearchPageTest.java
+++ src/com/smoketestcases/EbaySearchPageTest.java
@@ -1,4 +1,5 @@
 package com.smoketestcases;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -6,39 +7,35 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 public class EbaySearchPageTest {
-	
-public static WebDriver driver;
-	
+
+	public static WebDriver driver;
+
 	@BeforeClass
-	
-	public void OpenBrowser()
-	{
+
+	public void OpenBrowser() {
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.ebay.com/");
 	}
-	
+
 	@Test
-	
-	public void VerifySearchPage()
-	{
+
+	public void VerifySearchPage() {
 		WebElement SearchBox = driver.findElement(By.xpath("//input[@title='Search']"));
 		WebElement SearchIcon = driver.findElement(By.id("gh-search-btn"));
-		
+
 		SearchBox.sendKeys("Shirts");
 		SearchIcon.click();
-		
+
 		System.out.println(driver.getCurrentUrl());
 		System.out.println(driver.getTitle());
 	}
-	
-	
+
 	@AfterClass
-	
-	public void CloseBrowser()
-	{
+
+	public void CloseBrowser() {
 		driver.quit();
 	}
 }
-
